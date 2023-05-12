34c8a3baa4d03b94b7c8733a7adbbf08641103ad
@dmathcore231 <dmathcore231@gmail.com>
2023-05-12 15:41:59 +0300

    feat: added template todo(class)

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
 import { Modal } from 'bootstrap'
+// helpers
+function $(selector) {
+  return document.querySelector(selector)
+}
+// html elements
+const btnAddTodoElement = $('#btnAddTodo')
+const btnDelAllTodoElement = $('#btnDelAllTodo')
+const rowElement = $('.row')
+// modal add new todo
+const modalFormAddNewTodoElement = $('#modalFormAddNewTodo')
+const setTodoTitleElement = $('#setTodoTitle')
+const setTodoDescriptionElement = $('#setTodoDescription')
+const setTodoUserElement = $('#setTodoUser')
+const btnCloseModalAddTodoElement = $('#btnCloseModalAddTodo')
+const btnConfirmModalAddTodoElement = $('#btnConfirmModalAddTodo')
+// counter
+const counterActiveTodoElement = $('#counterActiveTodo')
+const counterInProgressElement = $('#counterInProgress')
+const counterDoneElement = $('#counterDone')
+// arr todo
+const todoList = []
+// class (template todo)
+class Todo {
+  id = crypto.randomUUID()
+  createdAt = new Date().toUTCString()
+  selectStatus = ['activeTodo', 'inProgress', 'done']
 
+  constructor(title = 'Title new Todo', description = 'My first Todo', user = 'Anonymous', valueSelectStatus = this.selectStatus[0]) {
+    this.title = title,
+      this.description = description,
+      this.user = user,
+      this.valueSelectStatus = valueSelectStatus
+  }
+}
+
+const test = new Todo()
+console.log(test)
 
